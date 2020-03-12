7b85d94d1f82458b0ba6722085064f8ad733e5cb
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-12 16:37:22 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.compents';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
