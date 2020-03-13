c62568cfd16e2c18962d0adf303e2f64106e073c
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-13 11:42:32 +0200

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.compents';
 import ShopPage from './pages/shoppage/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
