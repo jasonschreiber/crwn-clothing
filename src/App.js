8bc9140246dac6dc1631d942b1bd77132f1146ff
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-13 11:20:23 +0200

    updaing our app with a shop page, collection preview components and collection item component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.compents';
+import ShopPage from './pages/shoppage/shop.component';
 
-const HatsPage =() =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
