158104678ef07aaec741914c0714873e98f5fecd
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-24 13:40:49 +0200

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+import CartItem from '../../components/cart-item/cart-item.component';
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -27,7 +30,7 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem => 
-                cartItem.name    
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>    
             )
         }
         <div className='total'>
