967766441aad98d8ee525ffdbf15345258e58dc1
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-24 16:18:42 +0200

    added reducers for collections and homepage. seperated collections overview into its own component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,12 +1,27 @@
 //all reducers gointo the root reducer
 
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+
+const persistConfig ={
+    key: 'root',
+    storage,
+    whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
+
+});
 
 //keys for reducers
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
-})
+export default persistReducer(persistConfig, rootReducer);
