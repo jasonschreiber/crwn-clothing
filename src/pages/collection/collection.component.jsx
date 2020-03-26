3f79c94bb842acfc9df3dc5d8b18137b8da7c90b
jasonschreiber <jschreiber@esri-southafrica.com>
2020-03-26 12:32:47 +0200

    moved our directory and shop data into its own reducer. created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import {selectCollection} from '../../redux/shop/shop.selectors'; 
 
 import './collection.styles.scss';
 
-const CollectionPage = ({collection}) => (
-    <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
-        <h2>{collection.id}</h2>
-    </div>
-);
+const CollectionPage = ({collection}) => {
+    const { title, items } = collection;
+    return(
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+            {
+                items.map(item => <CollectionItem key={item.id} item={item}/>)
+            }
+            </div>
+        </div>
+    )
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
