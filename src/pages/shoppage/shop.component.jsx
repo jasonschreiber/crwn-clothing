0423ddc4602068f343a9a67d1c3fd440683fd734
jasonschreiber <48650385+jasonschreiber@users.noreply.github.com>
2020-04-07 17:25:43 +0200

    implement code that pulls in our shop data from firestore and stores

--- src/pages/shoppage/shop.component.jsx
+++ src/pages/shoppage/shop.component.jsx
@@ -26,12 +26,20 @@ class ShopPage extends React.Component {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({loading: false});
+        //promise pattern
+        collectionRef.get().then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+                updateCollections(collectionsMap);
+                this.setState({loading: false});  
+            })
 
-        });
+        //observer pattern
+        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({loading: false});
+
+        // });
     }
 
     render(){
