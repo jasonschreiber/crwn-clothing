bad9455af9d30bd88ce389fbee146b857fe34755
jasonschreiber <48650385+jasonschreiber@users.noreply.github.com>
2020-04-16 18:28:42 +0200

    added stripe server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axois from 'axios';
 
 
 import './stripe-button.styles.scss';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_vRMTxzXB3TUarBRAFthqTq6J007Nt9EjyX';
+    const publishableKey = 'pk_test_IOoC4SlAyMRH0ibQgyO0kUTs00pO0WWMS1';
 
     const onToken = token => {
-        console.log(token);
-        alert('Paymeny Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data:{
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card.');
+        })
     }
 
     return(
